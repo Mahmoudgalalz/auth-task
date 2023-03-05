14c33f835fe79e6ef5036a2b3531359e689e1d62
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-05 16:27:24 +0200

    import user and prisma module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersController } from './users/user.controller';
+import { UsersModule } from './users/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrisamService } from './prisma.service';
-import {User} from '@prisma/client'
 
 @Injectable()
 export class AppService {
-  constructor(private prisma: PrisamService){}
-  async createUser(userData): Promise<User>{
-    const result = await this.prisma.user.create({data:userData})
-    return result;
-  }
   getHello(): string {
     return 'Hello World!';
   }
