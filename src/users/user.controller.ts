605a50a178f229fc70b1bc8ed78b37425d6046c6
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-05 17:26:35 +0200

    create a GET endpoint on users

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './user.service';
 
 @Controller('account')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Post()
+    async create(@Body() createUsertDto: CreateUserDto){
+        return this.userService.create(createUsertDto)
+    }
+
+}
