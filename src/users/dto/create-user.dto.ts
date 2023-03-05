ade92ffe046965df5e0ec35af4e568d068f5823a
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-05 15:41:35 +0200

    Create user's DTO

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { Exclude, Transform } from "class-transformer";
+import { IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
+import { UserAddressBookDto } from "./user-addressbook.dto";
+
+export class CreateUserDto extends UserAddressBookDto {
+    @MinLength(3)
+    @Transform(({ value }) => (value as string).toLowerCase().replace(/[ ]/gi, '-'))
+    @Matches(/^[a-z0-9-]+$/, {
+      message: 'username can contain only lowercase characters, numbers and hyphens',
+    })
+    username: string;
+
+    @Exclude()
+    password:string;
+    
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+}
---
+++ src/users/dto/user-addressbook.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class UserAddressBookDto {
+    @IsEmail()
+    @IsString()
+    email:string;
+    
+    @IsString()
+    phone:string;
+
+    @IsString()
+    country: string;
+
+    @IsString()
+    city: string;
+
+    @IsString()
+    street: string;
+
+}
