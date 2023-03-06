d614ca7cb8e261e291b785411a9e2662fe8540ab
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-06 11:32:59 +0200

    Add ConfigModule and ConfigService to PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,23 @@
-import { INestApplication, Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, INestApplication } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    async enableShutdownHooks(app: INestApplication){
-        this.$on('beforeExit', async () => {
-            await app.close()
-        });
-    }
+  constructor(private config:ConfigService){
+    super(
+      {
+      datasources:{
+        db:{
+          url: config.get<string>('DATABASE_HOST')
+        }
+      }
+    });
+  }
+  
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close()
+    })
+  }
 }
