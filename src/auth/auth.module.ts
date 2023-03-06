72c74b9e9b77e7ebc352c3a8bb842f9b92823557
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-06 11:32:10 +0200

    Add PassportModule, JwtModule, JwtStrategy, UsersService and exports to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { UsersModule } from '../users/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersService } from 'src/users/user.service';
 
 @Module({
+  imports:[
+    PrismaModule,
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret:'SECRET_KEY',
+      signOptions: { expiresIn: '60day'}
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,JwtStrategy,UsersService],
+  exports:[AuthService],
 })
 export class AuthModule {}
