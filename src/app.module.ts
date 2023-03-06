c6e1df881793db96196737fbf9c1c9acfa095fba
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-06 11:34:09 +0200

    Update environment variables and packages, add AppController, ConfigModule, and ValidationPipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersController } from './users/user.controller';
-import { UsersModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    PrismaModule,
     UsersModule,
     AuthModule,
-    
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:'.env'
+    })
   ],
+  controllers:[AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api/v1')
+  
   Logger.log('App is running')
   await app.listen(3000);
 }
