e48e33ce4b30a0e3ab193383aac98bfe059fd678
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-06 11:30:31 +0200

    Update UpdateAuthDto to extend from CreateUserDto

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthDto } from './register.dto';
+import { CreateUserDto } from '../../users/dto/create-user.dto';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateUserDto) {}
